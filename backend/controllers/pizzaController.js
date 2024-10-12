005f1789dd3b88c6f4fb09e5610459f469bb97c7
Vithushayini <vithushayini2000@gmail.com>
2024-10-12 14:29:38 +0530

    add pizza, update pizza,delete pizza api created

--- backend/controllers/pizzaController.js
+++ backend/controllers/pizzaController.js
@@ -6,3 +6,59 @@ exports.getPizzas=async(req,res)=>{
     const pizzas= await Pizza.find();
     return res.status(200).json(pizzas);
 }
+
+//Add a new pizza-{{base_url}}/api/v1/menu
+exports.newPizza=async(req,res,next)=>{
+
+    const { name, description, price, imageUrl } = req.body;
+    const pizza=await Pizza.create({
+        name,
+        description,
+        price,
+        imageUrl
+    })
+
+    res.status(200).json({
+        success:true,
+        pizza
+    })
+}
+
+//Update pizza- {{base_url}}/api/v1/menu/:id
+exports.updatePizza=async(req,res,next)=>{
+    const {id}=req.params;
+    const { name, description, price, imageUrl } = req.body;
+    
+     // Find pizza by ID and update it
+     const updatedPizza = await Pizza.findByIdAndUpdate(
+        id,
+        { name, description, price, imageUrl },
+        { new: true } // Return the updated pizza
+    );
+
+    if (!updatedPizza) {
+        return res.status(404).json({ message: 'Pizza not found' });
+    }
+
+    res.json({ message: 'Pizza updated successfully!', pizza: updatedPizza });
+
+}
+
+//Delete Pizza-{{base_url}}/api/v1/menu/:id
+exports.deletePizza=async(req,res,next)=>{
+    const pizza=await Pizza.findById(req.params.id);
+
+    if(!pizza){
+        return res.status(404).json({
+            success:false,
+            message:"Pizza not found"
+        });
+    }
+
+    await pizza.deleteOne();
+
+    res.status(200).json({
+        success:true,
+        message:"Pizza Deleted"
+    })
+}
--- backend/routes/pizza.js
+++ backend/routes/pizza.js
@@ -1,9 +1,25 @@
 const express= require('express');
-const { getPizzas } = require('../controllers/pizzaController');
+const { getPizzas, newPizza, updatePizza, deletePizza } = require('../controllers/pizzaController');
 const router= express.Router();
+const {isAuthenticatedAdmin}=require('../middlewares/authenticate');
+const multer=require('multer');
+const path=require('path');
+
+// const upload=multer({storage: multer.diskStorage({
+//     destination:function(req, file, cb){
+//         cb(null,path.join(__dirname,'..' , 'uploads/product'))
+//     },
+//     filename:function(req,file,cb){
+//         cb(null,file.originalname)
+//     }
+// }) })
 
 router.route('/menu').get(getPizzas);
 
+//admin only routes
+router.route('/menu').post(isAuthenticatedAdmin,newPizza);
+router.route('/menu/:id').put(isAuthenticatedAdmin,updatePizza);
+router.route('/menu/:id').delete(isAuthenticatedAdmin,deletePizza);
 
 
 module.exports=router;
